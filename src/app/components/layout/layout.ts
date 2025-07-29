30c083b673c164f3e861f3dde8b110a668a210d2
Cristian Cajiao <cristiancajiaos@outlook.com>
2025-07-29 15:35:57 -0400

    Definido layout a usar

---
+++ src/app/components/layout/layout.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-layout',
+  standalone: false,
+  templateUrl: './layout.html',
+  styleUrl: './layout.scss'
+})
+export class Layout implements OnInit {
+
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+    
+  }
+
+}
